d9fc5962060bc7b9fdbf8b72dc1012a5d073d861
gzc <jackiegao6@gmail.com>
2025-08-21 20:48:45 +0800

    feat: 引入rabbitmq

---
+++ tiny-mall-pro-v1-trigger/src/main/java/com/gzc/trigger/listener/TeamSuccessTopicListener.java
@@ -0,0 +1,28 @@
+package com.gzc.trigger.listener;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TeamSuccessTopicListener {
+
+
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "${spring.rabbitmq.config.consumer.topic_team_success.queue}"),
+                    exchange = @Exchange(value = "${spring.rabbitmq.config.consumer.topic_team_success.exchange}", type = ExchangeTypes.TOPIC),
+                    key = "${spring.rabbitmq.config.consumer.topic_team_success.routing_key}"
+            )
+    )
+    public void listener(String msg){
+        log.info("支付商城接收消息: {}", msg);
+
+    }
+}
